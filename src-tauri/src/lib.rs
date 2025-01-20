b294e18ed83e2f1c8a7837d83c9ff411ea36ed4d
breducar <breducar7@gmail.com>
2025-01-20 11:14:31 -0300

    corrige erro de digitação

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,7 +21,7 @@ async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
     update
       .download_and_install(
         |chunk_length, content_length| {
-          downloaded += chunk_  length;
+          downloaded += chunk_length;
           println!("downloaded {downloaded} from {content_length:?}");
         },
         || {
